d62c34c8fdb9a89d6659d4d6cadb11979237ef54
Emmanuel Morales <emorales@emoneyadvisor.com>
2018-03-14 11:03:43 -0500

    Update dist manually

--- microfrontend1/src/app.module.js
+++
@@ -1,8 +0,0 @@
-import * as angular from 'angular';
-import { AppComponent } from './app.component';
-
-export const html = '<app></app>';
-
-export const AppModule = angular.module('app', [])
-    .component('app', AppComponent.component)
-    .name;
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { Application, render } from './application';
-
-document.addEventListener('DOMContentLoaded', () => {
-    render(Application, document.getElementById('app'));
-});
