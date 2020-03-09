5afe4dac757c5b5c3aaa1c99c8f37674b2b5518f
TrevorGitHub <notre_poubelle@yahoo.com>
2020-03-09 13:27:01 -0700

    Remove html imports.  Switch to including webcomponent js

--- microfrontend1/webpack.prod.js
+++ microfrontend1/webpack.prod.js
@@ -1,20 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     output: {
         path: path.resolve(__dirname, '../dist/microfrontend1'),
-        filename: '[name].[chunkhash].js'
+        filename: '[name].js'
     },
-    devtool: 'source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            chunks: ['main'],
-            filename: 'index.html',
-            template: path.resolve(__dirname, 'src/angular-app.html'),
-            inject: false
-        })
-    ]
+    devtool: 'source-map'
 });
